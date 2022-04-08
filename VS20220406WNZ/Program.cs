569efcf9699d2a34d5059d0d4c946b90918e3ac9
Walter <nz57@qq.com>
2022-04-08 20:48:23 +1200

    first commit

---
+++ VS20220406WNZ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VS20220406WNZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
