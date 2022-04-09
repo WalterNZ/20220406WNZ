14d339862346b1c2a72ca9b818802e35ae961cfb
Walter <nz57@qq.com>
2022-04-09 16:04:09 +1200

    second commit

--- VS20220406WNZ/Program.cs
+++ VS20220406WNZ/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 
 namespace VS20220406WNZ
 {
@@ -6,7 +8,36 @@ namespace VS20220406WNZ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // open chrome browser
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            // launch turnup protal website
+            driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+            // identify username textbox and enter valid username
+            IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+            usernameTextbox.SendKeys("hari");
+
+            // identify password textbox and enter valid password
+            IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+            passwordTextbox.SendKeys("123123");
+
+            // click on the login button
+            IWebElement loginbutton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+            loginbutton.Click();
+
+            // check if user is logged in successfully
+            IWebElement helloHari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+
+            if (helloHari.Text == "Hello hari!")
+            {
+                Console.WriteLine("Logged in successfully, test passed.");
+            }
+            else
+            {
+                Console.WriteLine("Login failed, test failed.");
+            }
         }
     }
 }
