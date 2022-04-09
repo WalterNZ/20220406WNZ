250ad6a0d9a711b18867017904f7ee2feb50e18f
Walter <nz57@qq.com>
2022-04-09 19:38:09 +1200

    3rd commit

--- VS20220406WNZ/Program.cs
+++ VS20220406WNZ/Program.cs
@@ -1,11 +1,14 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Threading;
 
 namespace VS20220406WNZ
 {
     internal class Program
     {
+        private static object creatnewbutton;
+
         static void Main(string[] args)
         {
             // open chrome browser
@@ -38,6 +41,64 @@ namespace VS20220406WNZ
             {
                 Console.WriteLine("Login failed, test failed.");
             }
+
+            // creat time and mertial record
+
+            // go to the T&M page
+            IWebElement adminDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+            adminDropdown.Click();
+
+            IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            tmOption.Click();
+
+            // click on the create new button
+            IWebElement creatnewButton = driver.FindElement(By.XPath("//*[@id='container']/p/a"));
+            creatnewButton.Click();
+
+            // select Material from Typecode dropdown
+            IWebElement typecodeDropdown = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[1]/div/span[1]/span/span[2]/span"));
+            typecodeDropdown.Click();
+            Thread.Sleep(1000);
+
+            IWebElement mOption = driver.FindElement(By.XPath("//*[@id='TypeCode_listbox']/li[1]"));
+            mOption.Click();
+
+            // Identify the code textbox and input a code
+            IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+            codeTextbox.SendKeys("20200406WNZ");
+
+            ;// Identify the description textbox and input a description
+            IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+            descriptionTextbox.SendKeys("20200406WNZ");
+
+            // Identify the price textbox and input a price
+            IWebElement abovePrice = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+            abovePrice.Click();
+
+            IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+            priceTextbox.SendKeys("20200406");
+
+            // Click on the Save button
+            IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+            saveButton.Click();
+            Thread.Sleep(1000);
+
+            // Click on the go to next page button
+            IWebElement gotolastpageButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+            gotolastpageButton.Click();
+            Thread.Sleep(1000);
+
+            // Check if record create is present in the table with expected value
+            IWebElement actualCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+
+            if (actualCode.Text == "20200406WNZ")
+            {
+                Console.WriteLine("Material record created successfully, test passed again!");
+            }
+            else
+            {
+                Console.WriteLine("Test Failed!");
+            }
         }
-    }
+    } 
 }
