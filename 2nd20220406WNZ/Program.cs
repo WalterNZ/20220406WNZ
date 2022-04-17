03e713b2d36c1c4265df5b8c5f8cfa7161700639
WalterNZ <nz57@qq.com>
2022-04-17 21:50:48 +1200

    2nd commit02-Creat new material record

--- 2nd20220406WNZ/Program.cs
+++ 2nd20220406WNZ/Program.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Threading;
 
 namespace _2nd20220406WNZ
 {
@@ -38,6 +39,64 @@ namespace _2nd20220406WNZ
             {
                 Console.WriteLine("Login failed, test failed.");
             }
+
+            // Creat time and material record
+
+            // Go to TM page
+            IWebElement adminDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+            adminDropdown.Click();
+
+            IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            tmOption.Click();
+
+            // Click on the createnew button
+            IWebElement createnewButton = driver.FindElement(By.XPath("//*[@id='container']/p/a")); 
+            createnewButton.Click();
+
+            // Select material from Typecode dropdown
+            IWebElement typecodeDropdown = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[1]/div/span[1]/span/span[2]/span"));
+            typecodeDropdown.Click();
+            Thread.Sleep(1000);
+
+            IWebElement materialOption = driver.FindElement(By.XPath("//*[@id='TypeCode_listbox']/li[1]"));
+            materialOption.Click();
+            Thread.Sleep(1000);
+
+            // Identify the codetextbox and input a code
+            IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+            codeTextbox.SendKeys("2nd@$#@21");
+
+            // Identify the descriptiontextbox and input description
+            IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+            descriptionTextbox.SendKeys("2nd@$#@21");
+
+            // Identify the pricetextbox and input a price
+            IWebElement priceTag = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+            priceTag.Click();
+            IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+            priceTextbox.SendKeys("2288666");
+
+            // Click save button
+            IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+            saveButton.Click();
+            Thread.Sleep(1000);
+
+            // Click go to the last page button
+            IWebElement gotolastpageButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+            gotolastpageButton.Click();
+
+            // Check if record created is present in the table with expected value
+            IWebElement actualCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+
+            if (actualCode.Text == "2nd@$#@21")
+            {
+                Console.WriteLine("Material record created successfully, test passed.");
+            }
+            else
+            {
+                Console.WriteLine("Record Creation failed, test failed.");
+            }
+
         }
     }
 }
